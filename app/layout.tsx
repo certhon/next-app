4570366359aea3c69ecbca991948db458c52cb73
chenhong <chenhong@soulapp.cn>
2024-09-24 13:38:04 +0800

    设置关键词

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 export const metadata = {
   title: '礼乘官网',
   description: '礼乘公司官方网站',
+  keywords: '礼乘, 礼乘财税, 公司注册,注册公司,灵活用工,代理记账,财税服务, 财税咨询, 财税代理, 财税顾问, 财税规划, 财税咨询服务, 财税代理服务, 财税顾问服务, 财税规划服务',
+
   icons: {
     icon: '/logo.ico',
   },
