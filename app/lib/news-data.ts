f349629d079652ba3262268d536d79a6409a27bc
chenhong <chenhong@soulapp.cn>
2026-08-26 09:02:24 +0800

    [feature][知识频道周更：公司名称变更怎么办？流程、材料和改名后必须联动的事]

--- app/lib/news-data.ts
+++ app/lib/news-data.ts
@@ -2,6 +2,7 @@
 // 列表页与详情页从这里读取；新文章加在数组最前面即可排在最新
 
 import type { Article, ArticleBlock } from './articles/types';
+import { article as gongsiMingchengBiangeng } from './articles/gongsi-mingcheng-biangeng';
 import { article as jingyingFanweiZenmeXie } from './articles/jingying-fanwei-zenme-xie';
 import { article as gongzhuansiHegui } from './articles/gongzhuansi-hegui';
 import { article as gongshangNianbao } from './articles/gongshang-nianbao';
@@ -56,6 +57,7 @@ import { article as wangdianYingyeZhizhao } from './articles/wangdian-yingye-zhi
 export type { Article, ArticleBlock };
 
 export const articles: Article[] = [
+  gongsiMingchengBiangeng,
   jingyingFanweiZenmeXie,
   gongzhuansiHegui,
   gongshangNianbao,
